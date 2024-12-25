d326354ed7774f48fd5b17a6e9e2a369ac1916b4
Ed <edtaghiyev@gmail.com>
2024-12-25 16:32:23 +0400

    Shitty shell

---
+++ shell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+
+    while (1)
+    {
+        printf("#cisfun$ ");
+        nread = getline(&line, &len, stdin);
+
+        if (nread == -1)
+        {
+            free(line);
+            exit(0);
+        }
+
+        line[strcspn(line, "\n")] = 0;
+
+        pid = fork();
+
+        if (pid == 0)
+        {
+            execlp(line, line, NULL);
+            perror("execve");
+            exit(1);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+            exit(1);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    free(line);
+    return 0;
+}
