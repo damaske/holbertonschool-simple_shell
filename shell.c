1498d4080025ff6e90f61044c77222f4551ce269
Ahmed <edtaghiyev@gmail.com>
2024-12-26 16:02:09 +0400

    update

--- shell.c
+++ shell.c
@@ -2,52 +2,54 @@
 
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-	int status;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
 
-	while (1)
-	{
-		printf("simple_shell$ ");
+    while (1)
+    {
+        printf("simple_shell$ ");
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-		{
-			printf("\n");
-			break;
-		}
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            printf("\n");
+            break;
+        }
 
-		pid = fork();
-		
-		if (pid == -1)
-		{
-			perror("fork");
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0)
-		{
-			char *args[2];
-			args[0] = line;
-			args[1] = NULL;
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-			if (execve(line, args, NULL) == -1)
-			{
-				perror("execve");
-				free(line);
-				exit(EXIT_FAILURE);
-			
-			}
-		}
-		else 
-		{
-			wait(&status);
-		}
-	}
-	free(line);
-	return (0);
+        pid = fork();
+        
+        if (pid == -1)
+        {
+            perror("fork");
+            free(line);
+            exit(EXIT_FAILURE);
+        }
+        
+        if (pid == 0)
+        {
+            char *args[2];
+            args[0] = line;
+            args[1] = NULL;
+
+            if (execve(line, args, NULL) == -1)
+            {
+                perror("execve");
+                free(line);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else 
+        {
+            wait(&status);
+        }
+    }
+
+    free(line);
+    return (0);
 }
