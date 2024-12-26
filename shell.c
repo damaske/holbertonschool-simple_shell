88d3adbc4de2fd93d4a4033c2752adccdd07d3a6
Ahmed <edtaghiyev@gmail.com>
2024-12-26 18:00:56 +0400

    Update

--- shell.c
+++ shell.c
@@ -11,47 +11,50 @@ int main(void)
     size_t len = 0;
     ssize_t nread;
     pid_t pid;
+    int i;
     int status;
     char *token;
+    char *args[64];
 
     while (1)
     {
+        printf("$ ");
         nread = getline(&line, &len, stdin);
         if (nread == -1)
         {
             break;
         }
-
         line[strcspn(line, "\n")] = 0;
-
+        i = 0;
         token = strtok(line, " ");
         while (token != NULL)
         {
-            pid = fork();
-            if (pid == -1)
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            free(line);
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            if (execve(args[0], args, NULL) == -1)
             {
-                perror("fork");
+                perror("execve");
                 free(line);
                 exit(EXIT_FAILURE);
             }
-            if (pid == 0)
-            {
-                char *args[2];
-                args[0] = token;
-                args[1] = NULL;
-
-                if (execve(token, args, NULL) == -1)
-                {
-                    perror("execve");
-                    free(line);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else
-            {
-                wait(&status);
-            }
-            token = strtok(NULL, " ");
+        }
+        else
+        {
+            wait(&status);
         }
     }
 
