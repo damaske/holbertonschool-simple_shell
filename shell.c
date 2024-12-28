9da3e4d72d9e2cf68580d75d10108fe93dfb92c9
Ahmed <edtaghiyev@gmail.com>
2024-12-28 21:26:20 +0400

    Update

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@ extern char **environ;
 int main(void) {
     char *buffer = NULL;
     char *newline;
+    char *cmd;
     size_t bufsize = 0;
 
     while (1) {
@@ -12,18 +13,26 @@ int main(void) {
             free(buffer);
             break;
         }
-
+        
         newline = strchr(buffer, '\n');
-        if (newline) *newline = '\0';
-        if (buffer[0] == '\0') continue;
+        if (newline)
+            *newline = '\0';
+        
+        if (buffer[0] == '\0')
+            continue;
+
+        cmd = strtok(buffer, " \t\n");
+        if (cmd == NULL)
+            continue;
 
         if (fork() == 0) {
             char *args[2];
-            args[0] = buffer;
+            args[0] = cmd;
             args[1] = NULL;
-            execve(buffer, args, environ);
-            perror("execve");
-            _exit(EXIT_FAILURE);
+            if (execve(cmd, args, environ) == -1) {
+                perror("execve");
+                _exit(EXIT_FAILURE);
+            }
         } else {
             wait(NULL);
         }
